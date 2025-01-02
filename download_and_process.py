6ddac559727b1b023f7829ed1566872cc58c8f10
Mikel Larreategi <mlarreategi@codesyntax.com>
2025-01-02 20:23:48 +0100

    improve code

--- download_and_process.py
+++ download_and_process.py
@@ -3,57 +3,92 @@ import re
 import urllib.request
 import os
 
-url = "https://s3.itbatera.euskadi.eus/02-pro-e3525cfb1b3d99109c5220a2b24bcb30-inet/transport/moveuskadi/data-index-gtfs.json"
+# GTFS URLs
 
-url_realtime = "https://s3.itbatera.euskadi.eus/02-pro-e3525cfb1b3d99109c5220a2b24bcb30-inet/transport/moveuskadi/data-index-gtfs-rt.json"
+## EAE
+url_eae_gtfs = "https://s3.itbatera.euskadi.eus/02-pro-e3525cfb1b3d99109c5220a2b24bcb30-inet/transport/moveuskadi/data-index-gtfs.json"
+url_eae_realtime = "https://s3.itbatera.euskadi.eus/02-pro-e3525cfb1b3d99109c5220a2b24bcb30-inet/transport/moveuskadi/data-index-gtfs-rt.json"
 
 
-url_geofabrik = "https://download.geofabrik.de/europe/spain/pais-vasco-latest.osm.pbf"
+## Nafarroa
+url_irunea = "https://transitfeeds.com/p/transporte-urbano-comarcal-de-pamplona/499/latest/download"
+url_nafarroa = "https://datosabiertos.navarra.es/dataset/ebfb5edd-0cd9-4b31-b0d9-8bc2d25e2493/resource/d3c1c89a-5d4c-4c25-97b5-66da663d3691/download/gtfs.zip"
+
+## Pirinio Atlantikoak
+url_pirinio_atlantikoak = "https://www.pigma.org/public/opendata/nouvelle_aquitaine_mobilites/publication/pyrenees_atlantiques-aggregated-gtfs.zip"
 
 
-def download_osm():
-    """ download OSM file"""
-    sock = urllib.request.urlopen(url)
-    data = sock.read()
-    with open("eae/osm.pbf", 'wb') as fp:
-        fp.write(data)
+# OSM URLs
+url_geofabrik_eae = "https://download.geofabrik.de/europe/spain/pais-vasco-latest.osm.pbf"
+url_geofabrik_nafarroa = "https://download.geofabrik.de/europe/spain/navarra-latest.osm.pbf"
+url_geofabrik_akitania = (
+    "https://download.geofabrik.de/europe/france/aquitaine-latest.osm.pbf"
+)
 
-    print('OSM data downloaded and ready')
-
-def slugify(name):
-    """ remove spaces and special chars"""
-    return re.sub(r'\W+', '-', name).strip('-')
-
-def validate(filename):
-    """ validate wether the file is correct. Delete if incorrect"""
-    if not os.stat(filename).st_size:
-        # File is empty, let's delete this one...
-        os.remove(filename)
-        print(f'Is empty. Deleted file: {filename}')
-
-def download_gtfs():
-    """ download and unzip the GTFS formatted files"""
-
-    sock = urllib.request.urlopen(url)
-    json_data = json.load(sock)
-    for operator, items in json_data.get('data', {}).items():
-        for i, item in enumerate(items):
-            gtfs_url = item.get('url')
-            gtfs_sock = urllib.request.urlopen(gtfs_url)
-            raw_gtfs_data = gtfs_sock.read()
-            filename = f'eae/gtfs-{slugify(operator)}-{i}.zip'
-            with open(filename, 'wb') as fp:
-                fp.write(raw_gtfs_data)
-
-            validate(filename)
-
-            print(f"File downloaded: {slugify(operator)} {i}")
-
-    print("Transport data downloaded and ready")
+# Constants for real time GTFS files
 
 VEHICLE_POSITION = 1
 ALERTS = 2
 TRIP_UPDATES = 3
+
+
+def slugify(name):
+    """remove spaces and special chars"""
+    return re.sub(r"\W+", "-", name).strip("-")
+
+
+def validate(filename, file_type):
+    """validate wether the GTFS file is correct. Delete if incorrect"""
+    if not os.stat(filename).st_size:
+        # File is empty, let's delete this one...
+        os.remove(filename)
+        print(f"Is empty. Deleted file: {filename}")
+
+
+def download_file(url, file_type, output_filename_append='', suffix=''):
+    """ generic function to download and write files into disk """
+    sock = urllib.request.urlopen(url)
+    filename = url.split('/')[-1]
+    raw_data = sock.read()
+    full_filename = f"eae/{file_type}-{output_filename_append}-{filename}{suffix}"
+    with open(full_filename, "wb") as fp:
+        fp.write(raw_data)
+        print(f"File written: {full_filename}")
+        validate(full_filename, file_type)
+
+def download_osm():
+    """ download OSM files """
+    for url in [url_geofabrik_eae, url_geofabrik_nafarroa]:
+        download_file(url, 'osm')
+
+    print('OSM data downloaded and ready')
+
+
+def download_gtfs_files():
+    """ download all GTFS files"""
+    download_gtfs_eae()
+    download_gtfs_nafarroa()
+    # download_gtfs_pirinio_atlantikoak()
+
+def download_gtfs_eae():
+    """ download and unzip the GTFS formatted files"""
+    sock = urllib.request.urlopen(url_eae_gtfs)
+    json_data = json.load(sock)
+    for operator, items in json_data.get('data', {}).items():
+        for i, item in enumerate(items):
+            gtfs_url = item.get('url')
+            download_file(gtfs_url, "gtfs", f"{slugify(operator)}-{i}")
+
+    print("Transport data downloaded and ready")
+
+def download_gtfs_nafarroa():
+    """ download files from nafarroa"""
+    for i, url in enumerate([url_nafarroa, url_irunea]):
+        download_file(url, 'gtfs', f'nafarroa-{i}', suffix='zip')
+
+def download_gtfs_pirinio_atlantikoak():
+    """ download files from atlantic pyrenees"""
+    download_file(url_pirinio_atlantikoak, 'gtfs')
 
 
 def get_operator(item):
@@ -73,8 +108,38 @@ def get_file_format(item):
     elif filename.find("trip") != -1:
         return TRIP_UPDATES
 
+
+def build_updater(item):
+    """ build real time updater stanza from EAE information"""
+    operator = get_operator(item)
+    fileformat = get_file_format(item)
+    updater = {}
+    if fileformat == VEHICLE_POSITION:
+        updater = {
+            "type": "vehicle-positions",
+            "url": item.get('url'),
+            "feedId": operator,
+            "frequencySec": 60,
+        }
+    elif fileformat == ALERTS:
+        updater = {
+            "type": "real-time-alerts",
+            "frequencySec": 30,
+            "url": item.get('url'),
+            "feedId": operator,
+        }
+    elif fileformat == TRIP_UPDATES:
+        updater = {
+            "type": "stop-time-updater",
+            "frequencySec": 60,
+            "backwardsDelayPropagationType": "REQUIRED_NO_DATA",
+            "url": item.get('url'),
+            "feedId": operator,
+        }
+    return updater
+
 def download_and_build_rt_feed():
-    sock = urllib.request.urlopen(url_realtime)
+    sock = urllib.request.urlopen(url_eae_realtime)
     data = json.load(sock)
 
     rt_feed = {
@@ -84,40 +149,16 @@ def download_and_build_rt_feed():
 
     for _, items in data.get('data', {}).items():
         for item in items:
-            operator = get_operator(item)
-            fileformat = get_file_format(item)
-            updater = {}
-            if fileformat == VEHICLE_POSITION:
-                updater = {
-                    "type": "vehicle-positions",
-                    "url": item.get('url'),
-                    "feedId": operator,
-                    "frequencySec": 60,
-                }
-            elif fileformat == ALERTS:
-                updater = {
-                    "type": "real-time-alerts",
-                    "frequencySec": 30,
-                    "url": item.get('url'),
-                    "feedId": operator,
-                }
-            elif fileformat == TRIP_UPDATES:
-                updater = {
-                    "type": "stop-time-updater",
-                    "frequencySec": 60,
-                    "backwardsDelayPropagationType": "REQUIRED_NO_DATA",
-                    "url": item.get('url'),
-                    "feedId": operator,
-                }
+            updater = build_updater(item)
             if updater:
                 rt_feed['updaters'].append(updater)
 
     with open('eae/router-config.json', 'w') as fp:
         json.dump(rt_feed, fp)
 
-        print('router config written')
+        print('Real time router config written')
 
 if __name__ == '__main__':
     download_osm()
-    download_gtfs()
+    download_gtfs_files()
     download_and_build_rt_feed()
